42833362955c82f0d019667b548c4086ef9f334f
williams <oneal.wills@gmail.com>
2022-03-04 10:38:55 +0100

    Create-Place-Order-API

--- backend/utils.js
+++ backend/utils.js
@@ -14,3 +14,27 @@ export const generateToken = (user) => {
     }
   );
 };
+// midddleware accept 3 parameters => request, response, next
+export const isAuth = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (authorization) {
+    const token = authorization.slice(7, authorization.length); // Bearer XXXXXX
+    jwt.verify(
+      token,
+      process.env.JWT_SECRET || "somethingsecret",
+      //   callback function with 2 params error and the decode that contains the data
+      //   jwt.verify returns the data(username,email, isadmin, id) that was passed to generate token inside decode
+      (err, decode) => {
+        if (err) {
+          res.status(401).send({ message: "Invalid Token" });
+        } else {
+          // pass user as a property of req to the next middleware
+          req.user = decode;
+          next();
+        }
+      }
+    );
+  } else {
+    res.status(401).send({ message: "No Token" });
+  }
+};
